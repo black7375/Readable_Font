824a20fe648431dba975d651e15a55066c2e23b6
alstjr7375 <alstjr7375@daum.net>
2021-01-06 08:40:06 +0900

    Clean: delete files

--- src/popup/popup.ts
+++
@@ -1,9 +0,0 @@
-import { callbackT, callback_checkbox } from "../lib/checkbox";
-
-const id_callback = (callback: callbackT) => {
-  callback("font_sharpen");
-  callback("font_substitue");
-  callback("font_math");
-}
-
-callback_checkbox(id_callback);
